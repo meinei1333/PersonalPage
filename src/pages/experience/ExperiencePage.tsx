3f2493b31734ae875e5cfa4cd2b2aa52fbafc3c8
Chao Taphy <taphylili@gmail.com>
2021-11-15 19:43:02 +0800

    experiencePage useTranslation

--- src/pages/experience/ExperiencePage.tsx
+++ src/pages/experience/ExperiencePage.tsx
@@ -3,39 +3,40 @@ import styles from "./ExperiencePage.module.css"
 import { Header } from "../../components"
 import { Table } from 'antd';
 import { list } from './experienceData'
-
-const columns = [
-    {
-        title: '公司名稱',
-        dataIndex: 'name',
-        key: 'name',
-        width: 50.
-    },
-    {
-        title: '職位',
-        dataIndex: 'position',
-        key: 'position',
-    },
-    {
-        title: '期間',
-        dataIndex: 'duration',
-        key: 'duration',
-    },
-    {
-        title: '工作內容',
-        dataIndex: 'content',
-        key: 'content',
-        render: (text:any, record:any) => (
-            <div style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
-              {text}
-            </div>
-          ),
-    },
-];
-
-
+import { useTranslation } from "react-i18next";
 
 export const ExperiencePage: React.FC = () => {
+    const { t } = useTranslation();
+
+    const columns = [
+        {
+            title: t("experiencePage.companyName"),
+            dataIndex: 'name',
+            key: 'name',
+            width: 50.
+        },
+        {
+            title: t("experiencePage.position"),
+            dataIndex: 'position',
+            key: 'position',
+        },
+        {
+            title: t("experiencePage.duration"),
+            dataIndex: 'duration',
+            key: 'duration',
+        },
+        {
+            title: t("experiencePage.content"),
+            dataIndex: 'content',
+            key: 'content',
+            render: (text: any, record: any) => (
+                <div style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
+                    {text}
+                </div>
+            ),
+        },
+    ];
+
     return <div className={styles["experience"]}>
         <Header></Header>
         <Table dataSource={list} columns={columns} pagination={false} />
